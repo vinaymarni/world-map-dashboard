4d1f360326b0ee42a2ed19b3dcef506e241ed739
vinaymarni <iamvinnu.m@gmail.com>
2022-02-24 09:56:49 +0530

    Wave motion icons

--- world-map/src/App.tsx
+++ world-map/src/App.tsx
@@ -1,75 +1,132 @@
 import { world_map } from './world-map';
-import { uncountries } from './data';
+//import { uncountries } from './data';
 import * as React from "react";
 import { MapsComponent, LayersDirective, LayerDirective, Inject, Legend, MapsTooltip, Zoom, Selection, MarkersDirective, MarkerDirective, Marker  } from '@syncfusion/ej2-react-maps';
+import Content from './Content';
+import './App.css'
+
+const SAMPLE_CSS = `
+.pulse-css {
+    width: 10px;
+    height: 10px;
+    -webkit-border-radius: 15px;
+    -moz-border-radius: 15px;
+    border-radius: 15px;
+    background:#3386dc;
+    position: relative;
+  }
+  .pulse-css:before, .pulse-css:after {
+    content: '';
+    width: 10px;
+    height: 10px;
+    -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    border-radius: 10px;
+    background-color: #3386dc;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    transform: scale(0.5);
+    transform-origin: center center;
+    animation: pulse-me 3s linear infinite;
+  }
+  .pulse-css:after {
+    animation-delay: 2s;
+  }
+  @keyframes pulse-me {
+    0% {
+      transform: scale(0.5);
+      opacity: 0;
+    }
+    50% {
+      opacity: 0.3;
+    }
+    70% {
+      opacity: 0.1;
+    }
+    100% {
+      transform: scale(5);
+      opacity: 0;
+    }
+  }
+    `; 
+  let marketTemp: string = '<div id="template"> <div class="pulse-container"><div class="pulse-box"><div class="pulse-css"></div></div></div></div>'
+
 class App extends React.Component {
     render() {
+        
         return (
-          <MapsComponent id="maps" legendSettings={{ visible: true }} theme="Material" zoomSettings={{
-            enable: true,
-            color: 'green',
-            highlightColor: 'blue',
-            selectionColor: 'orange',
-            horizontalAlignment: 'Far',
-            mouseWheelZoom: true,
-            toolbars: ['ZoomIn', 'ZoomOut', 'Pan', 'Reset']}}
-          >
-             <Inject services={[Legend, MapsTooltip, Zoom, Selection, Marker]}/>
-             <LayersDirective>
-                <LayerDirective 
-                  shapeData={world_map} 
-                  shapeDataPath='Country' 
-                  shapePropertyPath='name' 
-                  /*
-                  tooltipSettings={{
-                    visible: true,
-                    valuePath: 'name'
-                  }}
-                  */
-                  selectionSettings={{
-                    enable: true,
-                    fill: 'gray',
-                    border: { color: 'black', width: 1 }
-                }}
-                >
-                  <MarkersDirective> 
-                    <MarkerDirective 
-                      visible={true} 
-                      height={10} width={10} 
-                      fill="#3386dc" shape="Circle" 
-                      tooltipSettings={{
-                        visible: true,
-                        valuePath: 'city'
-                      }} 
-                      dataSource={[{ latitude: 49.95121990866204, longitude: 18.468749999999998, city: 'Poland'  }]}
-                    >
-                    </MarkerDirective>
-                    <MarkerDirective 
-                      visible={true} 
-                      height={10} width={10} 
-                      fill="#3386dc" shape="Circle" 
-                      tooltipSettings={{
-                        visible: true,
-                        valuePath: 'city'
-                      }}
-                      dataSource={[{ latitude: 59.88893689676585, longitude: -109.3359375, city: 'Canada' }]}
-                      >
-                    </MarkerDirective>
-                    <MarkerDirective 
-                    visible={true} 
-                    height={10} width={10} 
-                    fill="#3386dc" shape="Circle" 
+          <div className='main-container'>
+            <style>
+              {SAMPLE_CSS}
+            </style>
+            <div className='top-bar'>
+              <h1 className='heading'>World Map</h1>
+              <div>
+                <input list="inputs-drop-down" type="search" className="search" placeholder="Region"/>
+                <datalist className="drop-list"  id="inputs-drop-down">
+                  <option value="America" >America</option>
+                  <option value="India">India</option>
+                  <option value="Australia">Australia</option>
+                  <option value="Russia">Russia</option>
+                </datalist>
+              </div>
+            </div>
+            <MapsComponent id="maps" className='maps' legendSettings={{ visible: true }} theme="Material" zoomSettings={{
+              enable: true,
+              color: 'green',
+              highlightColor: 'blue',
+              selectionColor: 'orange',
+              horizontalAlignment: 'Far',
+              mouseWheelZoom: false,
+              toolbars: ['ZoomIn', 'ZoomOut', 'Pan', 'Reset']}} 
+            > 
+              <Inject services={[Legend, MapsTooltip, Zoom, Selection, Marker]}/>
+              <LayersDirective>
+                  <LayerDirective 
+                    shapeData={world_map} 
+                    shapeDataPath='Country' 
+                    shapePropertyPath='name' 
+                    /*
                     tooltipSettings={{
                       visible: true,
-                      valuePath: 'city'
+                      valuePath: 'name'
                     }}
-                    dataSource={[{ latitude: -6.64607562172573, longitude: -55.54687499999999, city: 'Brazil' }]}>
-                    </MarkerDirective>
-                  </MarkersDirective>
-                </LayerDirective>
-            </LayersDirective>
-        </MapsComponent>
+                    */
+                    animationDuration={500}
+                    selectionSettings={{
+                      enable: true,
+                      fill: 'gray',
+                      border: { color: 'black', width: 1 }
+                  }}
+                  
+                  >
+                    <h1 style={{color: 'red'}}>World Map</h1>
+                    <MarkersDirective> 
+                      <MarkerDirective 
+                        visible={true} 
+                        height={10} width={10} 
+                        template = {marketTemp}
+                        tooltipSettings={{
+                          visible: true,
+                          valuePath: 'city'
+                        }} 
+                        dataSource={[
+                          { latitude: 20.95121990866204, longitude: 73.468749999999998, city: <Content />  },
+                          { latitude: 50.88893689676585, longitude: -80.3359375, city: 'Canada' },
+                          { latitude: -14.64607562172573, longitude: 35.54687499999999, city: 'Malawi' }
+                        ]}
+                        >
+                      </MarkerDirective>
+                    </MarkersDirective>
+                  </LayerDirective>
+              </LayersDirective>
+            </MapsComponent>
+          </div>
           );
     }
 }
-export default App;
+export default App; 
---
+++ world-map/src/Content.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Content extends React.Component {
+    public render(){
+        return(
+            <div>
+                <h1>Healthy</h1>
+            </div>
+        )
+    }
+}
+
+export default Content
